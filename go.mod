3a9ccbe5bdaa02c17bd776024ae4bb261b5eaa1e
user <user@user.com>
2023-11-29 01:02:59 +0800

    [+] test

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.21.3
 
 require (
 	github.com/chroblert/jarr v0.1.0
-	github.com/chroblert/jasync v0.1.0
+	github.com/chroblert/jasync v0.1.2
 	github.com/chroblert/jlog v0.0.9
 	github.com/chroblert/jstr v0.1.1
 )
@@ -12,6 +12,7 @@ require (
 require (
 	github.com/hashicorp/go-uuid v1.0.3 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
-	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sync v0.5.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
 )
--- jcsv/encoder_test.go
+++ jcsv/encoder_test.go
@@ -1,6 +1,8 @@
 package jcsv
 
 import (
+	"fmt"
+	"github.com/chroblert/jasync"
 	"github.com/chroblert/jlog"
 	"testing"
 )
@@ -19,6 +21,15 @@ func TestQuoteField(t *testing.T) {
 	//	jlog.NInfo("test1," + field)
 	//} else {
 	//}
+	a := jasync.NewAR(1000)
+	for i := 0; i < 10000; i++ {
+		a.Init(fmt.Sprintf("task-%d", i)).CAdd(func(i int) {
+			jlog.NInfof("test1,test%d\n", i)
+			word_list := DecodeString2List(fmt.Sprintf("test1,test%d\n", i))
+			jlog.Info(word_list)
+		}, i).CDO()
+	}
+	a.Wait()
 	jlog.Info("test1," + EncodeField(field))
 	word_list := DecodeString2List("test1,test2")
 	jlog.Info(word_list)
